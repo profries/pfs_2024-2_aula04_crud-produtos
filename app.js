89ff87dca81f4ab588fd3b75add988f05b58669f
LUIS HENRIQUE LEAL RIES <LHRIES@fspoa.br>
2024-08-28 21:56:51 -0300

    Passo 3 e 4 - Tratando req e res

--- app.js
+++ app.js
@@ -2,29 +2,48 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json());
+
+const produtos = [
+  {id:1, nome:"feijao", preco:10, categoria:"alimento"},
+  {id:2, nome:"arroz", preco:8, categoria:"alimento"},
+  {id:3, nome:"suco de laranja", preco:12, categoria:"bebida"},
+];
+
+app.get('/', (req, res) => {  
+  res.send("Hello World!");
 })
 
 app.get('/produtos', (req, res) => {
-    res.send('Listar Produtos!')
+  res.json(produtos);
 })
 
 app.get('/produtos/:id', (req, res) => {
-  res.send('Buscar produto por id!')
+  const id = req.params.id;
+
+  res.json(produtos[0]);
 })
 
   
 app.post('/produtos', (req, res) => {
-  res.send('Inserir Produto!')
+  const produto = req.body;
+  console.log(produto);
+  
+  res.status(201).json(produtos[0]);
 })
 
 app.put('/produtos/:id', (req, res) => {
-  res.send('Atualizar produto!')
+  const id = req.params.id;
+  const produto = req.body;
+  console.log(produto);
+
+  res.json(produtos[0]);
 })
 
 app.delete('/produtos/:id', (req, res) => {
-  res.send('Deletar produto!')
+  const id = req.params.id;
+
+  res.json(produtos[0]);
 })
 
 
