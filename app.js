3189300b2092c20de0d713458edb4552a482aa12
LUIS HENRIQUE LEAL RIES <LHRIES@fspoa.br>
2024-09-04 22:15:51 -0300

    Adicionado as funcionalidades com BD

--- app.js
+++ app.js
@@ -1,49 +1,136 @@
 const express = require('express')
+const { Client } = require('pg');
+
+const conexao = {
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: '123456',
+  database: 'crud_produtos'
+}
+
 const app = express()
 const port = 3000
 
 app.use(express.json());
 
 const produtos = [
-  {id:1, nome:"feijao", preco:10, categoria:"alimento"},
-  {id:2, nome:"arroz", preco:8, categoria:"alimento"},
-  {id:3, nome:"suco de laranja", preco:12, categoria:"bebida"},
+  { id: 1, nome: "feijao", preco: 10, categoria: "alimento" },
+  { id: 2, nome: "arroz", preco: 8, categoria: "alimento" },
+  { id: 3, nome: "suco de laranja", preco: 12, categoria: "bebida" },
 ];
 
-app.get('/', (req, res) => {  
+app.get('/', (req, res) => {
   res.send("Hello World!");
 })
 
-app.get('/produtos', (req, res) => {
-  res.json(produtos);
+app.get('/produtos', async (req, res) => {
+  const client = new Client(conexao);
+  await client.connect();
+
+  const result = await client.query(
+    "SELECT * FROM PRODUTOS"
+  );
+  const listaProdutos = result.rows;
+  await client.end();
+  res.json(listaProdutos);
 })
 
-app.get('/produtos/:id', (req, res) => {
+app.get('/produtos/:id', async (req, res) => {
   const id = req.params.id;
 
-  res.json(produtos[0]);
+  const client = new Client(conexao);
+  await client.connect();
+  const result = await client.query(
+    "SELECT * FROM PRODUTOS WHERE id=$1", [id]
+  );
+  const produto = result.rows[0];
+  await client.end();
+
+  if (produto) {
+    res.json(produto);
+  }
+  else {
+    res.status(404)
+      .json({ erro: "Produto nao encontrado" });
+  }
 })
 
-  
-app.post('/produtos', (req, res) => {
+
+app.post('/produtos', async (req, res) => {
   const produto = req.body;
-  console.log(produto);
-  
-  res.status(201).json(produtos[0]);
+
+  if (!produto || !produto.nome || !produto.categoria
+    || !produto.preco) {
+    res.status(400).json(
+      { erro: "Informacoes de produto incompletas!" })
+  }
+  else {
+    const client = new Client(conexao);
+    await client.connect();
+    const result = await client.query(
+      `INSERT INTO PRODUTOS (nome, categoria, preco) 
+      VALUES ($1, $2, $3) RETURNING *`,
+      [produto.nome, produto.categoria, produto.preco]
+    );
+    const produtoInserido = result.rows[0];
+    await client.end();
+
+    res.status(201).json(produtoInserido);
+  }
 })
 
-app.put('/produtos/:id', (req, res) => {
+app.put('/produtos/:id', async (req, res) => {
   const id = req.params.id;
   const produto = req.body;
-  console.log(produto);
 
-  res.json(produtos[0]);
+  if (!produto || !produto.nome || !produto.categoria
+    || !produto.preco) {
+    res.status(400).json(
+      { erro: "Informacoes de produto incompletas!" })
+  }
+  else {
+    const client = new Client(conexao);
+    await client.connect();
+    const result = await client.query(
+      `UPDATE PRODUTOS
+      SET nome=$1, categoria=$2, preco=$3
+      WHERE id=$4 RETURNING *`,
+      [produto.nome, produto.categoria, produto.preco, id]
+    );
+    const produtoAtualizado = result.rows[0];
+    await client.end();
+
+    if (produtoAtualizado) {
+      res.json(produtoAtualizado);
+    }
+    else {
+      res.status(404)
+        .json({ erro: "Produto nao encontrado" });
+    }
+  }
+
 })
 
-app.delete('/produtos/:id', (req, res) => {
+app.delete('/produtos/:id', async (req, res) => {
   const id = req.params.id;
 
-  res.json(produtos[0]);
+  const client = new Client(conexao);
+  await client.connect();
+  const result = await client.query(
+    "DELETE FROM PRODUTOS WHERE id=$1 RETURNING *", [id]
+  );
+  const produtoDeletado = result.rows[0];
+  await client.end();
+
+  if (produtoDeletado) {
+    res.json(produtoDeletado);
+  }
+  else {
+    res.status(404)
+      .json({ erro: "Produto nao encontrado" });
+  }
+
 })
 
 
